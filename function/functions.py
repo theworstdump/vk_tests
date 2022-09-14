40f4533e77b4a54fec99016384db350c42ec6f52
DMITRYY NAZAREV <nazarev1995@mail.ru>
2022-09-14 18:19:27 +0300

    Fixed #2

--- function/functions.py
+++ function/functions.py
@@ -1,13 +1,23 @@
 def func_1(x: int):
     return x * x
 
-def func_2(text: str):
+def func_2(text: str) -> str:
     return text.upper()
 
 def func_3(number: int) -> int:
     return number % 3
 
+#Функция возвращает True, если переданная строка написана с заглавной буквы, и False, если первая буква строчная
+def func_4(text: str) -> bool:
+    if (text[0] == text[0].upper()):
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
     print(func_3(7))
     print(func_3(-100))
+    print(func_4('Abc'))
+    print(func_4('bc'))
 
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,5 +1,5 @@
 import pytest
-from function.functions import func_1, func_2, func_3
+from function.functions import func_1, func_2, func_3, func_4
 
 def test_1():
     assert func_1(3) == 9
@@ -9,6 +9,10 @@ def test_2():
 @pytest.mark.parametrize("number, remain", [(-102, 0), (-101, 1), (-100, 2), (0, 0), (100, 1), (101, 2), (102, 0)])
 def test_3(number, remain):
     assert func_3(number) == remain
+
+@pytest.mark.parametrize("input_text, result", [('ABC', True), ('AbC', True), ('aBC', False), ('abc', False)])
+def test_4(input_text, result):
+    assert func_4(input_text) == result
 
 if __name__ == '__main__':
     pytest.main()
